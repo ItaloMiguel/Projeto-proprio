62a8807b31fb4eb788df5213b40339b65d19ae23
ItaloMiguel <italomiguelleite16@gmail.com>
2022-10-30 11:14:50 -0300

    "Corrigindo bug no Modularizando o Projeto"

--- course/src/main/java/br/com/microsservice/course/CursoApplication.java
+++ course/src/main/java/br/com/microsservice/course/CursoApplication.java
@@ -2,8 +2,12 @@ package br.com.microsservice.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan({"br.com.microsservice.core.model"})
+@EnableJpaRepositories({"br.com.microsservice.core.repository"})
 public class CursoApplication {
 
 	public static void main(String[] args) {
